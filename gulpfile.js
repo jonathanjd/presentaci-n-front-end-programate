abd52584528386d10c594772ca9947e37d667b81
Jonathan Duran <headjd@gmail.com>
2018-02-09 22:50:09 -0400

    Inicio

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp        = require("gulp");
+var browserSync = require("browser-sync").create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
