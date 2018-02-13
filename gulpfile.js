644fa9e4ef564e116614d579365d7bdb1ec87660
Jonathan Duran <headjd@gmail.com>
2018-02-12 22:04:18 -0400

    Clase 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-var gulp        = require("gulp");
+var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', function() {
-
+gulp.task("serve", function() {
     browserSync.init({
-        server: "./"
+        server: "./",
+        port: 8000, //Or whatever port you want for your application
+        ui: {
+            port: 8001 //Or whatever port you want for browsersync ui
+        }
     });
 
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./*.html").on("change", browserSync.reload);
 });
